cb50e728b217891aabadc9320a43778e124ef429
Lorien <lorien@luocynet.com>
2026-06-09 16:54:31 +0800

    Change: add GPLv3 headers (26 orig + 3 new), delete config.json.example, add config.toml.example

--- src/handler.go
+++ src/handler.go
@@ -1,3 +1,16 @@
+// *******************************************************************
+// src/handler.go
+//
+// Copyright (C) 2024 iEdon
+// Copyright (C) 2026 Luochancy
+//
+// This file is part of a project derived from peerapi-agent.
+// Modified by Luochancy on 2026-06.
+//
+// Licensed under the GNU General Public License v3.0.
+// See the LICENSE file in the project root for details.
+// *******************************************************************
+
 package main
 
 import (
