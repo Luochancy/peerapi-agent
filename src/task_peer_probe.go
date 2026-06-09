5d37db990fe880524e9d13063330837ec46cff90
Lorien <lorien@luocynet.com>
2026-06-09 16:45:32 +0800

    Revert "Change: add GPLv3 copyright headers to all source files (27 orig + 3 new)"
    This reverts commit 4c68ea1c5976b18c7d84fbe04aaef8eb116c9d3e.

--- src/task_peer_probe.go
+++ src/task_peer_probe.go
@@ -1,16 +1,3 @@
-// *******************************************************************
-// src/task_peer_probe.go
-//
-// Copyright (C) 2024 iEdon
-// Copyright (C) 2026 Luochancy
-//
-// This file is part of a project derived from peerapi-agent.
-// Modified by Luochancy on 2026-06.
-//
-// Licensed under the GNU General Public License v3.0.
-// See the LICENSE file in the project root for details.
-// *******************************************************************
-
 //go:build linux
 
 package main
